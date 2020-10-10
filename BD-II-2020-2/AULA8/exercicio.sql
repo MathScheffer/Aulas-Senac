887ebabf930c39415a73524c3f93aa023f3c4f57
mathscheffer <you@example.com>
2020-10-10 17:59:21 -0300

    refactor(exercicio.sql): adicionar comentários referentes aos exercícios para melhor organização do código.

--- BD-II-2020-2/AULA8/exercicio.sql
+++ BD-II-2020-2/AULA8/exercicio.sql
@@ -1,19 +1,15 @@
---Usando RECORD
 /*
-drop function fn_lista()
-CREATE OR REPLACE FUNCTION fn_lista() RETURNS SETOF RECORD AS
-$$
-BEGIN
-	RETURN QUERY SELECT nome, totalmultas FROM EX_MOTORISTA;
-	return;
-END;
-$$
-LANGUAGE PLPGSQL;
-
-select nome, totalmultas FROM fn_lista() AS (nome varchar(20), totalmultas decimal)
+Monte um procedimento o qual possa gerenciar as
+mensagens de erro fm_msg() o qual receba um tipo de erro e
+gere uma respectiva mensagem;
 */
 
---Retornando uma table
+
+/*
+• Crie um procedimento fm_lista() o qual liste os motoristas
+(nome) e seu total de multas, caso o parametro sejam
+TODOS ou apenas de um CNH;*/
+
 drop function fn_lista(char(5))
 
 CREATE OR REPLACE FUNCTION fn_lista( param char(5) ) RETURNS TABLE(n varchar(20), tm decimal) AS
@@ -45,6 +41,13 @@ LANGUAGE PLPGSQL;
 select fn_lista('123ac') --AS (nome varchar(20), totalmultas decimal)
 
 
+/*
+• Monte um procedimento fm_totalMultas() o qual retorne o
+total de multas de cada motorista. Use na função fm_lista()
+*/
 
-
-
+/*
+• OBS: use o procedimento fm_msg() para que gere
+mensagens do tipo:
+– Motorista não cadastrado;
+– Motorista sem multas, etc*/
